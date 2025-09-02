714dab8eb3c1f5e75c145bed7decb8a8ee1fca5a
Aveneli <aveneli.loja@gmail.com>
2025-09-02 18:52:53 -0300

    Update server.js

--- server.js
+++ server.js
@@ -271,9 +271,3 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor rodando na porta ${PORT}`);
 });
-
-// ================= START =================
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`🚀 Servidor rodando na porta ${PORT}`);
-});
